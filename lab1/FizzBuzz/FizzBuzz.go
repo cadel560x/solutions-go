9fedd447e45ebe4c0dbe83c9c6ac4d6dba71fade
JAVIER MANTILLA - Student <G00329649@gmit.ie>
2017-09-26 21:42:03 +0100

    New version inspired on http://wiki.c2.com/?FizzBuzzTest

--- lab1/FizzBuzz/FizzBuzz.go
+++ lab1/FizzBuzz/FizzBuzz.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
+	var output string
+
 	for i := 1; i < 101; i++ {
-		switch {
-		case i%3 == 0 && i%5 == 0:
-			fmt.Println("FizzBuzz")
-		case i%3 == 0:
-			fmt.Println("Fizz")
-		case i%5 == 0:
-			fmt.Println("Buzz")
-		default:
-			fmt.Println(i)
-		} // switch
+		if i%3 == 0 {
+			output = "Fizz"
+		}
+
+		if i%5 == 0 {
+			output += "Buzz"
+		}
+
+		if len(output) == 0 {
+			output = strconv.Itoa(i)
+		}
+
+		fmt.Println(output)
+
+		output = ""
+
 	} // for
 } // main
