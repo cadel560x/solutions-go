a43eae2adb6c7ed2c5084c90e38788d95c10d947
JAVIER MANTILLA - Student <G00329649@gmit.ie>
2017-10-06 18:03:32 +0100

    Final edit Palindrome

--- lab1/Palindrome/Palindrome.go
+++ lab1/Palindrome/Palindrome.go
@@ -1,9 +1,53 @@
 package main
 
 import (
+	"bytes"
+	"container/list"
 	"fmt"
 )
 
 func main() {
+	input := ""
+
+	fmt.Print("Please enter a word: ")
+	fmt.Scanln(&input)
+
+	fmt.Printf("Is %s palindrome: %t\n\n", input, palindrome(input))
 
 } // main
+
+func palindrome(testString string) bool {
+	// I didn't find a native, out of the box implementation of a stack in Go
+	// So, let's use a linked list a stack
+	aStack := list.New()
+
+	var stringBuilder bytes.Buffer
+
+	// Push 'testString' into 'aStack'
+	for i := 0; i < len(testString); i++ {
+		// Convert 'string' characters into 'runes' because Go is so cool. Wow!
+		aStack.PushBack(rune(testString[i]))
+	}
+
+	// Debug
+	// fmt.Print("Debug: palindrome(): ")
+	// for e := aStack.Front(); e != nil; e = e.Next() {
+	// 	fmt.Printf("%c", e.Value.(rune))
+	// }
+	// fmt.Print("\n")
+
+	// Pops out the stack into a 'bytes.Buffer' (like Java's StringBuilder)
+	for e := aStack.Back(); e != nil; e = e.Prev() {
+		// Saw this in StackOverflow https://stackoverflow.com/questions/1760757/how-to-efficiently-concatenate-strings-in-go
+		stringBuilder.WriteRune(e.Value.(rune))
+	}
+
+	// Debug
+	// fmt.Println("Debug: palindrome(): stringBuilder: ", string(stringBuilder.String()))
+
+	if testString == stringBuilder.String() {
+		return true
+	}
+
+	return false
+}
