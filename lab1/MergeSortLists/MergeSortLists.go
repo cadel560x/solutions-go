dfaec26cb168fa7b26af61bb2aa2067da9ad3e9e
cadel560x <necoclister@gmail.com>
2017-10-06 22:43:24 +0100

    MergeSort implementation

--- lab1/MergeSortLists/MergeSortLists.go
+++ lab1/MergeSortLists/MergeSortLists.go
@@ -3,60 +3,59 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
-	// Create two lists
-	// aList := new(list.List)
-	// bList := new(list.List)
-	var aList []int8
-	var bList []int8
 
-	// Populate them with a random amount of ordered random numbers
-	populateList(&aList)
-	populateList(&bList)
-
-	// Print the lists
-	fmt.Println("aList: ", aList)
-	// printList(aList)
-	fmt.Print("\n")
-	fmt.Print("bList: ", bList)
-	// printList(bList)
-
+	slice := generateSlice(50)
+	fmt.Println("\n --- unsorted --- \n\n", slice)
+	fmt.Println("\n--- sorted ---\n\n", mergeSort(slice), "\n")
 } // main
 
-func populateList(lista *[]int8) {
-	// Set the seed for the random source
-	// s1 := rand.NewSource(time.Now().UnixNano())
+// Generates a slice of size, size filled with random numbers
+func generateSlice(size int) []int {
 
-	// Debug
-	randomLength := uint8(rand.Intn(9) + 1)
-	// randomLength := uint8(rand.New(s1).Intn(9) + 1)
-	fmt.Println("Debug: populateList: randomLength: ", randomLength)
-
-	*lista = make([]int8, randomLength, randomLength)
-
-	for index, value := range *lista {
-		// Generate negative and positive integer numbers between -128 to 127
-		// Taken from StackOverflow: https://stackoverflow.com/questions/3938992/how-to-generate-random-positive-and-negative-numbers-in-java
-
-		// Debug
-		randomNumber := int8(rand.Intn(255) - 128)
-		// randomNumber := int8(rand.New(s1).Intn(255) - 128)
-		fmt.Println("Debug: populateList: for loop: randomNumber ", randomNumber)
-
-		for randomNumber < value {
-			randomNumber = int8(rand.Intn(255) - 128)
-		}
-
-		lista(index) = randomNumber
-		// lista.PushBack(randomNumber)
+	slice := make([]int, size, size)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		slice[i] = rand.Intn(99999) - rand.Intn(99999)
 	}
-} // populateList
+	return slice
+} // generateSlice
 
-func printList(lista []int8) {
-	for _, value := range lista {
-		fmt.Print(value, " -> ")
+// Runs MergeSort algorithm on a slice single
+func mergeSort(slice []int) []int {
+
+	if len(slice) < 2 {
+		return slice
 	}
-	fmt.Println("END")
-} // printList
+	mid := (len(slice)) / 2
+	return merge(mergeSort(slice[:mid]), mergeSort(slice[mid:]))
+} // mergeSort
+
+// Merges left and right slice into newly created slice
+func merge(left, right []int) []int {
+
+	size, i, j := len(left)+len(right), 0, 0
+	slice := make([]int, size, size)
+
+	for k := 0; k < size; k++ {
+		if i > len(left)-1 && j <= len(right)-1 {
+			slice[k] = right[j]
+			j++
+		} else if j > len(right)-1 && i <= len(left)-1 {
+			slice[k] = left[i]
+			i++
+		} else if left[i] < right[j] {
+			slice[k] = left[i]
+			i++
+		} else {
+			slice[k] = right[j]
+			j++
+		} // if
+
+	} // for
+
+	return slice
+} // merge
