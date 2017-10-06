4417a0174ef2d0a228c49e57efc87b7f70151ad2
cadel560x <necoclister@gmail.com>
2017-10-06 22:12:08 +0100

    Last edit NewtonSqrt

--- lab1/NewtonSqrt/NewtonSqrt.go
+++ lab1/NewtonSqrt/NewtonSqrt.go
@@ -5,18 +5,33 @@ import (
 	"math"
 )
 
-func sqrt(x float64) float64 {
+func newtonSqrt(x float64) float64 {
+	// Square root of 0 is 0
 	if x == 0 {
 		return 0
 	}
+
+	// Starting point
 	z := 1.0
+
 	for i := 0; i < int(x); i++ {
 		z = z - ((z*z - x) / (2 * z))
 	}
 	return z
-}
+} // newtonSqrt
 
 func main() {
-	fmt.Println(sqrt(2))
-	fmt.Println(math.Sqrt(2))
-}
+	// Debug
+	// fmt.Println(sqrt(2))
+	// fmt.Println(math.Sqrt(2))
+
+	numbers := 20
+	for i := 0; i < numbers; i++ {
+		mathSqrt := math.Sqrt(float64(i))
+		newton := newtonSqrt(float64(i))
+		fmt.Println(i, "square root:")
+		fmt.Println("  math.Sqrt:", mathSqrt)
+		fmt.Println("  Newton's method:", newton)
+		fmt.Println("  Difference:", math.Abs(mathSqrt-newton))
+	}
+} // main
